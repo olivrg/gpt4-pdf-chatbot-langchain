15fc9f38e9de53d1081e39bf9232448e73261612
Oli Guei <olivrg@gmail.com>
2023-05-11 13:36:43 +0100

    Configure makechain to ingest docs

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,8 +9,8 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+const QA_PROMPT = `You are a helpful product manual AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know and instruct the user to contact the product's support service. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
